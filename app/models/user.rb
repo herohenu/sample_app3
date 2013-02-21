3e6229ae54a3e6fcf32aa9efd2a7d4c124132b60
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-02-21 16:07:31 +0800

    添加email唯一索引

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,14 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  before_save { |user| user.email = email.downcase }
+
    ###6.2.2      验证存在性
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-
+  #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221071559) do
+ActiveRecord::Schema.define(:version => 20130221080042) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,5 @@ ActiveRecord::Schema.define(:version => 20130221071559) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 end
