63b73fb3fe11857b11e166dd6cd6bac526f427c3
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-02-23 21:09:43 +0800

    9.5end  完成用户编辑更新删除操作

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  ##code9.12
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,6 +13,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+
+  #def create
+  #  @user = User.new(params[:user])
+  #  if @user.save
+  #    sign_in @user
+  #
+  #    # Handle a successful save.
+  #    flash[:success] = "Welcome to the Sample App!"
+  #    redirect_to @user
+  #  else
+  #    render 'new'
+  #  end
+  #end
 
   def create
     @user = User.new(params[:user])
@@ -21,14 +45,22 @@ class UsersController < ApplicationController
     end
   end
 
+  ################9.46
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+
   ###################################code 9.2
   def edit
-    @user = User.find(params[:id])
+   @user = User.find(params[:id])
   end
 
   ############################code9.8    ##code9.10
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -37,4 +69,23 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  ##code9.12###############################################
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
 end
