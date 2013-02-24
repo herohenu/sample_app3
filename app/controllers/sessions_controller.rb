eaae21e0b38586c16c6b4312cbb0610bc2900bdc
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-02-24 17:39:34 +0800

    10.3.3 临时的动态列表

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,4 +30,16 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
 end
