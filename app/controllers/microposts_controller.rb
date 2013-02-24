7a044dc94049c53830dd48b30d5ce8593f060aed
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-02-24 17:47:44 +0800

    10.3.4 end test 5 errors

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
   before_filter :signed_in_user
 
   def create
@@ -13,5 +15,14 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
