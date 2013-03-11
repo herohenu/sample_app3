ec122ea53a20d8d2cceb9631a786021be6ff24d0
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-03-11 17:58:26 +0800

    application.sj

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130223131705) do
     t.boolean  "admin",           :default => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
